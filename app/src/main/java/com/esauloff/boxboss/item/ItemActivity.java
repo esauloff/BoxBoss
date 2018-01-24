6f1ff6f46c488f5657b697970a567144c5221577
Georgii Iesaulov <esauloff@gmail.com>
2018-01-24 00:02:22 -0500

    Add ItemEditor empty activity
    Signed-off-by: Georgii Iesaulov <esauloff@gmail.com>

--- app/src/main/java/com/esauloff/boxboss/item/ItemActivity.java
+++ app/src/main/java/com/esauloff/boxboss/item/ItemActivity.java
@@ -1,7 +1,9 @@
 package com.esauloff.boxboss.item;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.esauloff.boxboss.R;
 
@@ -11,6 +13,11 @@ public class ItemActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+    }
+
+    public void openItemEditor(View view) {
+        Intent intent = new Intent(this, ItemEditorActivity.class);
+        startActivity(intent);
     }
 }
 
---
+++ app/src/main/java/com/esauloff/boxboss/item/ItemEditorActivity.java
@@ -0,0 +1,16 @@
+package com.esauloff.boxboss.item;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.esauloff.boxboss.R;
+
+public class ItemEditorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_editor);
+    }
+}
+
